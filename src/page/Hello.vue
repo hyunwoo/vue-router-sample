df12d1550e0e022e2dbf610f59d8bfc249e8bc02
user <user@userui-MacBook-Pro.local>
2018-09-19 20:01:56 +0900

    svg example added

--- src/page/Hello.vue
+++ src/page/Hello.vue
@@ -1,32 +1,103 @@
 <template lang="pug">
   .hello
-    h3 WT?
-    .card-group
-      template(v-for="card in cards")
-        card(:cardName="card.name")
+    input(v-model.number="circle[0].cx")
+    input(v-model.number="circle[0].cy")
+    div
+    svg(width="100%" height="300px" w)
+      circle(v-for="c in circle" v-bind="c")
 
 </template>
 
 <script>
 
 import Vue from 'vue';
+import * as _ from 'lodash';
+import MixinBase from '../components/mixin-base';
 import { store } from '../vuex/store';
 import card from '../components/card';
+import Common from '../util/common';
 
+
+Vue.prototype.$util = Common;
+Vue.prototype.$_ = _;
+
+// mixin Vue 생명주기와 관련하여 TODO 가 있는 경우 사용한다.
+// library , util 기능이 필요한경우에는 mixin 이 아닌 prototype 을 사용한다.
 Vue.component('card', card);
 
 export default {
-  name: 'HelloWorld',
   store,
+  mixins: [MixinBase],
+  mounted() {
+    for (let i = 0; i < 20; i += 1) {
+      this.circle.push({
+        cx: 80 * i,
+        cy: 0,
+        r: 50,
+        type: 'red',
+        data: 'w'
+      });
+    }
+    setInterval(() => {
+      for (let i = 0; i < 20; i += 1) {
+        setTimeout(() => {
+          this.circle[i].cy = 100;
+          this.circle[i].fill = '#cb3745';
+        }, 50 * i);
+      }
+    }, 2000);
+
+    setTimeout(() => {
+      setInterval(() => {
+        for (let i = 0; i < 20; i += 1) {
+          setTimeout(() => {
+            this.circle[i].cy = 300;
+            this.circle[i].fill = '#e99a2b';
+          }, 50 * i);
+        }
+      }, 2000);
+    }, 1000);
+
+  },
   data() {
     return {
+      circle: [{
+        cx: 100,
+        cy: 100,
+        r: 50,
+        type: 'red',
+        data: 'w'
+      }, {
+        cx: 300,
+        cy: 100,
+        r: 50,
+        type: 'red',
+        data: 't'
+      }, {
+        cx: 500,
+        cy: 100,
+        r: 50,
+        type: 'red',
+        data: 'f'
+      }],
+      testNumber: '10000000',
       msg: 'Welcome to Hanu Vue Template',
-      cards: []
+      cards: [],
+      name: '',
+      age: '',
+      gender: '',
+      testObject: {}
     };
   },
   computed: {
+    getDisplayText() {
+      return `${this.name}은 나이가 ${this.age}인 ${this.gender}입니다`;
+    },
     count() {
       return this.$store.state.count;
+    },
+    getTestObject() {
+      return this.testObject;
     },
     getPosition() {
       return {
@@ -36,6 +107,11 @@ export default {
     }
   },
   methods: {
+    removeText(i, k) {
+      delete this.testObject[i][k];
+      // Vue.delete(this.testObject[i], k);
+      console.log(this.testObject);
+    },
     increment() {
       this.$store.commit('increment', { inc: 1 });
     },
@@ -50,22 +126,33 @@ export default {
   created: function () {
     // todo get data from server
     console.log(this);
-
+    this.baseMethod();
     setTimeout(() => {
       for (let i = 0; i < 5; i += 1) {
         this.cards.push({ name: `name${i}` });
       }
     }, 1000);
+    for (let i = 0; i < 2; i += 1) {
+      this.testObject[`${i}`] = { a: 10, b: 20, c: 30 };
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
+svg
+  circle
+    transition: all .3s
+  &[w]
+    circle[data=t]
+      r: 30px !important
+
 li
-  background : #333
-  color : #fff
+  background: #333
+  color: #fff
   text-decoration: none
+
 .card-group
   display: flex
 
